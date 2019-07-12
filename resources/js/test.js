3cb209d6b169763bace942e69fa534f7e2f3cd8d
lisaschumann <44737067+lisaschumann@users.noreply.github.com>
2019-07-11 18:23:38 -0700

    New.

---
+++ resources/js/test.js
@@ -0,0 +1,44 @@
+let listArray = [];
+
+  // ui variables
+  const form = document.querySelector(".form"); 
+  const input = document.querySelector(".input");
+  const ul = document.querySelector(".name-list"); 
+
+  // event listeners
+  form.addEventListener('submit', e => {
+    // prevent default behaviour - Page reload
+    e.preventDefault();
+    // give item a unique ID
+    let itemId = String(Date.now());
+    // get/assign input value
+    let toDoItem = input.value;
+    //pass ID and item into functions
+    addItemToDOM(itemId , toDoItem);
+    addItemToArray(itemId, toDoItem);
+    // clear the input box. (this is default behaviour but we got rid of that)
+    input.value = '';
+  });
+
+   // function
+   function addItemToDOM(itemId, toDoItem) {    
+    // create an li
+    const li = document.createElement('li')
+    li.setAttribute("data-id", itemId);
+    // add toDoItem text to li
+    li.innerText = toDoItem
+    // add li to the DOM
+    ul.appendChild(li);
+  }
+
+  function addItemToArray(itemId, toDoItem) {
+    // add item to array as an object with an ID so we can find and delete it later
+    listArray.push({ itemId, toDoItem});
+    console.log(listArray)
+  }
+
+  console.log('Lisa');
+  console.log(input);
+
+
+
