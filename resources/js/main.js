ab56284f2d893ec9c033f5f9010a11a4e9cc7e7e
Lisa Schumann <44737067+lisaschumann@users.noreply.github.com>
2019-10-30 15:17:59 +0000

    New blog addition.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,113 @@
+// Navigation Anchor with correct margin to display sections
+        (function (document, history, location) {
+            var HISTORY_SUPPORT = !!(history && history.pushState);
+
+            var anchorScrolls = {
+                //ANCHOR_REGEX: /^#[^ ]+$/
+                ANCHOR_REGEX: /^[\/a-z0-9-.]*#[^\s]+$/,
+                OFFSET_HEIGHT_PX: 58,
+
+                /**
+                 * Establish events, and fix initial scroll position if a hash is provided.
+                 */
+                init: function () {
+                    this.scrollToCurrent();
+                    window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
+                    document.body.addEventListener('click', this.delegateAnchors.bind(this));
+                },
+
+                /**
+                 * Return the offset amount to deduct from the normal scroll position.
+                 * Modify as appropriate to allow for dynamic calculations
+                 */
+                getFixedOffset: function () {
+                    return this.OFFSET_HEIGHT_PX;
+                },
+
+                /**
+                 * If the provided href is an anchor which resolves to an element on the
+                 * page, scroll to it.
+                 * @param  {String} href
+                 * @return {Boolean} - Was the href an anchor.
+                 */
+                scrollIfAnchor: function (href, pushToHistory) {
+                    var match, rect, anchorOffset;
+
+                    if (!this.ANCHOR_REGEX.test(href)) {
+                        return false;
+                    }
+
+                    match = document.getElementById(href.slice(1));
+
+                    if (match) {
+                        rect = match.getBoundingClientRect();
+                        anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
+                        window.scrollTo(window.pageXOffset, anchorOffset);
+
+                        // Add the state to history as-per normal anchor links
+                        if (HISTORY_SUPPORT && pushToHistory) {
+                            history.pushState({}, document.title, location.pathname + href);
+                        }
+                    }
+
+                    return !!match;
+                },
+
+                /**
+                 * Attempt to scroll to the current location's hash.
+                 */
+                scrollToCurrent: function () {
+                    this.scrollIfAnchor(window.location.hash);
+                },
+
+                /**
+                 * If the click event's target was an anchor, fix the scroll position.
+                 */
+                delegateAnchors: function (e) {
+                    var elem = e.target;
+
+                    if (
+                        elem.nodeName === 'A' &&
+                        this.scrollIfAnchor(elem.getAttribute('href'), true)
+                    ) {
+                        e.preventDefault();
+                    }
+                }
+            };
+
+            window.addEventListener(
+                'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
+            );
+        })(window.document, window.history, window.location);
+
+// Portfolio
+
 var portfolioItems = [{
+        url: 'https://www.quite-something.de',
+        imageUrl: './resources/images/quite-something.jpg',
+        client: 'Quite Something',
+        imagePositon: 'center',
+        subtitle: 'Independent Magazin from Munich',
+        paragraph: 'Website maintenance and shop integrations',
+        portfolioSite: 'https://www.quite-something.de'
+    },
+    {
         url: 'https://www.matildaeyre.com',
         imageUrl: './resources/images/matildaeyre.jpg',
         client: 'Matilda Eyre',
         imagePositon: 'center',
         subtitle: 'Custom website for independent artist',
         paragraph: 'Soundcloud integration and music portfolio',
-        portfolioSite: '/portfolio/matildaeyre.html'
+        portfolioSite: 'https://www.matildaeyre.com'
     },
     {
         url: 'https://www.anderl-heckmair.com',
         imageUrl: './resources/images/mark-basarab-157610-unsplash.jpg',
         client: 'Anderl Heckmair',
         imagePositon: 'center',
-        subtitle: 'Custom website and shop integration',
-        paragraph: 'Beautiful bavarian outdoor fashion.'
+        subtitle: 'Brand website and shop integration',
+        paragraph: 'Beautiful bavarian outdoor fashion.',
+        portfolioSite: 'https://www.anderl-heckmair.com'
     },
     {
         url: 'https://www.brydiemusic.com',
@@ -21,7 +115,8 @@ var portfolioItems = [{
         client: 'Brydie Music',
         imagePositon: 'right',
         subtitle: 'DJ & Producer website',
-        paragraph: 'More info coming soon.'
+        paragraph: 'More info coming soon.',
+        portfolioSite: 'https://www.brydiemusic.com'
     },
     {
         url: 'http://der-wohlstands-trainer.de',
@@ -29,7 +124,8 @@ var portfolioItems = [{
         client: 'Der Wohlstands Trainer',
         imagePositon: 'right',
         subtitle: 'Fully customized coaching website',
-        paragraph: 'More info coming soon.'
+        paragraph: 'More info coming soon.',
+        portfolioSite: 'http://der-wohlstands-trainer.de'
     },
     {
         url: 'https://www.löwenherz.tv',
@@ -37,7 +133,8 @@ var portfolioItems = [{
         client: 'Löwenherz TV',
         imagePositon: 'center',
         subtitle: 'Custom website for a German coach',
-        paragraph: 'More info coming soon.'
+        paragraph: 'More info coming soon.',
+        portfolioSite: 'https://www.löwenherz.tv'
     },
     {
         url: 'https://lisaschumann.github.io/colmar_academy/',
@@ -45,7 +142,8 @@ var portfolioItems = [{
         client: 'Colmar Academy',
         imagePositon: 'center',
         subtitle: 'Educational project page',
-        paragraph: 'More info coming soon.'
+        paragraph: 'More info coming soon.',
+        portfolioSite: 'https://lisaschumann.github.io/colmar_academy/'
     },
     {
         url: 'https://lisaschumann.github.io/u4_tea_cozy/',
@@ -53,7 +151,8 @@ var portfolioItems = [{
         client: 'Tea Cozy',
         imagePositon: 'center',
         subtitle: 'Commercial example',
-        paragraph: 'More info coming soon.'
+        paragraph: 'More info coming soon.',
+        portfolioSite: 'https://lisaschumann.github.io/u4_tea_cozy/'
     },
     {
         url: 'https://lisaschumann.github.io/u4_my_times/',
@@ -61,7 +160,8 @@ var portfolioItems = [{
         client: 'My Times',
         imagePositon: 'center',
         subtitle: 'Example of a news page',
-        paragraph: 'More info coming soon.'
+        paragraph: 'More info coming soon.',
+        portfolioSite: 'https://lisaschumann.github.io/u4_my_times/'
     },
     {
         url: 'https://www.ann-christin-osteopathy.com/',
@@ -69,7 +169,8 @@ var portfolioItems = [{
         client: 'Osteopathy',
         imagePositon: 'center',
         subtitle: 'Client base and booking.',
-        paragraph: 'More info coming soon.'
+        paragraph: 'More info coming soon.',
+        portfolioSite: 'https://www.ann-christin-osteopathy.com/'
     }
 ]
 
@@ -118,39 +219,44 @@ window.addEventListener('scroll',() => {
 
 // Set a variable for our button element.
 const scrollToTopButton = document.getElementById('js-top');
+smoothScrollCapture = (x) => {
+  event.preventDefault();
+  var id = event.target.getAttribute('href').split("#")[1]
+  var el = document.getElementById(id);
+  var x = el.getBoundingClientRect().left
+  var y = el.getBoundingClientRect().top
+  smoothScrollFunc(x, y)
+}
 
-// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
-const scrollFunc = () => {
+smoothScrollFunc = (x, y) => {
+  event.preventDefault()
+  window.scrollTo({
+    top: y,
+    left: x,
+    behavior: 'smooth'
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  scrollToTopButton.addEventListener('click', () => {
+    smoothScrollFunc(0, 0)
+  })
+
+  var aTags = document.getElementsByTagName('A')
+  var myTags = []
+  for (var i = 0; i < aTags.length; i++) {
+    if (aTags[i].getAttribute("href").indexOf('#') === 0) {
+      aTags[i].addEventListener('click', smoothScrollCapture);
+    }
+  }
+})
+
+window.addEventListener('scroll', () => {
   // Get the current scroll value
   let y = window.scrollY;
-  
-  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
   if (y > 100) {
     scrollToTopButton.className = "top-link show";
   } else {
     scrollToTopButton.className = "top-link hide";
   }
-};
-
-window.addEventListener("scroll", scrollFunc);
-
-const scrollToTop = () => {
-  // Let's set a variable for the number of pixels we are from the top of the document.
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
-  
-  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
-  // We'll also animate that scroll with requestAnimationFrame:
-  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
-  if (c > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    // ScrollTo takes an x and a y coordinate.
-    // Increase the '10' value to get a smoother/slower scroll!
-    window.scrollTo(0, c - c / 10);
-  }
-};
-
-// When the button is clicked, run our ScrolltoTop function above!
-scrollToTopButton.onclick = function(e) {
-  e.preventDefault();
-  scrollToTop();
-}
+})
